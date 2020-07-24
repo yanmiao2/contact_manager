000a1ffac257db42a73e13aa5c746f3f692d967a
Yan Miao <yanmiao2@illinois.edu>
2020-07-24 16:52:15 -0500

    backend finished

--- server.js
+++ server.js
@@ -1,9 +1,17 @@
 const express = require('express');
+const connectDB = require('./config/db');
 
 const app = express();
 
-app.get('/', (req, res) => res.json({ msg: 'welcome to the api' }));
+// Connect Database
+connectDB();
 
+// Init Middleware
+app.use(express.json({ extended: false }));
+
+// app.get('/', (req, res) => res.json({ msg: 'welcome to the api' }));
+
+// Define Routes
 app.use('/api/users', require('./routes/users'));
 app.use('/api/auth', require('./routes/auth'));
 app.use('/api/contacts', require('./routes/contacts'));
